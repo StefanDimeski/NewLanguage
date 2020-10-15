dbf218bffa1e213f4e52911d4895fae8ecb608f7
StefanDimeski <stefandimeski90@gmail.com>
2020-10-15 22:27:56 +0200

    Wow, it works as a charm! To add: input node

--- lang.py
+++ lang.py
@@ -4,147 +4,88 @@ from enum import Enum
 
 class Node:
     def __init__(self):
-        self.children = []
-        self.terminal = False
-        self.reduceable = False
-
-    def initialize(self, value):
         pass
 
-    def add_children(self, children):
-        self.children.extend(children)
-
-    def add_child(self, child):
-        self.children.append(child)
-
-    def exec_self(self, symbol_table):
-        return None
-
     def exec(self, symbol_table):
-        for child in self.children:
-            child.exec(symbol_table)
-
-        return self.exec_self(symbol_table)
-
-class Main(Node):
-    def __init__(self):
-        super().__init__()
-
-class Statement(Node):
-    def __init__(self):
-        super().__init__()
-        self.reduceable = True
-
-class Expression(Node):
-    def __init__(self):
-        super().__init__()
-        self.reduceable = True
+        pass
 
 class Assignment(Node):
-    def __init__(self):
+    def __init__(self, var, expr):
         super().__init__()
+        self.var = var
+        self.expr = expr
 
     def exec(self, symbol_table):
-        ident = self.children[0].identifier
-        val = self.children[1].exec(symbol_table)
+        ident = self.var.identifier
+        val = self.expr.exec(symbol_table)
 
         symbol_table[ident] = val
 
         return None
 
 class If_Statement(Node):
-    def __init__(self):
+    def __init__(self, cond, block):
         super().__init__()
+        self.condition = cond
+        self.block = block
 
     def exec(self, symbol_table):
-        condition = self.children[0].exec(symbol_table)
+        condition = self.condition.exec(symbol_table)
         if not isinstance(condition, bool):
             return None
 
         if condition:
-            self.children[1].exec(symbol_table)
+            self.block.exec(symbol_table)
 
         return None
 
-class OperatorExpression(Node):
-    def __init__(self):
+class Operator(Node):
+    def __init__(self, left, op, right):
         super().__init__()
+        self.left = left
+        self.right = right
+        self.op = op
 
     def exec(self, symbol_table):
-        results = []
-        for node in self.children:
-            results.append(node.exec(symbol_table))
-
-        type = results[1]
-
-        if type == '+':
-            return results[0] + results[2]
-        elif type == '-':
-            return results[0] - results[2]
-        elif type == '==':
-            return results[0] == results[2]
-        elif type == '!=':
-            return results[0] != results[2]
-        elif type == '*':
-            return results[0] * results[2]
+        if self.op == '+':
+            return int(self.left.exec(symbol_table)) + int(self.right.exec(symbol_table))
+        elif self.op == '-':
+            return int(self.left.exec(symbol_table)) - int(self.right.exec(symbol_table))
+        elif self.op == '==':
+            return int(self.left.exec(symbol_table)) == int(self.right.exec(symbol_table))
+        elif self.op == '!=':
+            return int(self.left.exec(symbol_table)) != int(self.right.exec(symbol_table))
+        elif self.op == '*':
+            return int(self.left.exec(symbol_table)) * int(self.right.exec(symbol_table))
+        elif self.op == '/':
+            return int(self.left.exec(symbol_table)) / int(self.right.exec(symbol_table))
         else:
-            print(f'ERROR: Invalid operator type: "{type}"')
+            print(f'ERROR: Invalid operator type: "{self.op}"')
             return False
 
-class Operator(Node):
-    def __init__(self):
+class Var(Node):
+    def __init__(self, ident):
         super().__init__()
-        self.type = None
-        self.terminal = True
-
-    def initialize(self, value):
-        self.type = value
+        self.identifier = ident
 
     def exec(self, symbol_table):
-        return self.type
-
-class Value(Node):
-    def __init__(self):
-        super().__init__()
-        self.value = None
-        self.terminal = True
-
-    def initialize(self, value):
-        self.value = value
-
-    def exec_self(self, symbol_table):
-        return self.value
-
-class Var(Node):
-    def __init__(self):
-        super().__init__()
-        self.identifier = None
-        self.terminal = True
-
-    def initialize(self, value):
-        self.identifier = value
-
-    def exec_self(self, symbol_table):
         return symbol_table[self.identifier]
 
 class Number(Node):
-    def __init__(self):
+    def __init__(self, num):
         super().__init__()
-        self.num = None
-        self.terminal = True
+        self.num = int(num)
 
-    def initialize(self, value):
-        self.num = int(value)
-
-    def exec_self(self, symbol_table):
+    def exec(self, symbol_table):
         return self.num
 
 class Print(Node):
-    def __init__(self):
+    def __init__(self, expr):
         super().__init__()
+        self.expr = expr
 
     def exec(self, symbol_table):
-        res = self.children[0].exec(symbol_table)
+        res = self.expr.exec(symbol_table)
 
         print(res)
         #if isinstance(res, str):
@@ -155,8 +96,9 @@ class Print(Node):
         return None
 
 class Block(Node):
-    def __init__(self):
+    def __init__(self, children):
         super().__init__()
+        self.children = children
 
     def exec(self, symbol_table):
         # We create a new table such that we implement scoping
@@ -169,22 +111,28 @@ class Block(Node):
 
         return None
 
-regexes = ["^[a-zA-Z][0-9a-zA-Z]*$", "^[0-9][0-9]*$", "^if$", "^print$", "^=$", "^(\+|-|\*|/|==|!=)$",
+regexes = ["^[a-zA-Z][0-9a-zA-Z]*$", "^[0-9][0-9]*$", "^if$", "^print$", "^=$", "^(\+|-)$", "^(\*|/)$", "^(==|!=)$",
            "^\($", "^\)$", "^\{$", "^\}$", "^;$"]
 
-class Token(Enum):
+class TType(Enum):
     IDENT = 0
     NUMBER = 1
     IF = 2
     PRINT = 3
     EQUAL = 4
-    BIN_OP = 5
-    OPEN_BRACE = 6
-    CLOSED_BRACE = 7
-    OPEN_CURLY = 8
-    CLOSED_CURLY = 9
-    SEMICOLON = 10
+    BIN_OP_PLUS_MIN = 5
+    BIN_OP_MULT_DIV = 6
+    BIN_OP_EQ_NOTEQ = 7
+    OPEN_BRACE = 8
+    CLOSED_BRACE = 9
+    OPEN_CURLY = 10
+    CLOSED_CURLY = 11
+    SEMICOLON = 12
 
+class Token():
+    def __init__(self, type, val):
+        self.type = type
+        self.val = val
 # check out if300 - this is an identifier in C, so it's okay
 # but if+ is def not an identifier and it actually is caught only after
 # compiling so lexically it's okay, which is what i expected and how this
@@ -194,15 +142,29 @@ class Lexer():
     def __init__(self, text):
         self.text = text
         self.generator = self.gen_tokens()
+        self.next_token = None
 
     def eat(self, token_type):
-        nxt_type, val = next(self.generator)
 
-        if nxt_type != token_type:
-            print(f"ERROR: Invalid token {val}")
+        nxt = None
+        if self.next_token != None:
+            nxt = self.next_token
+            self.next_token = None
+        else:
+            nxt = next(self.generator)
+
+        if nxt.type != token_type:
+            print(f"ERROR: Invalid token {nxt.val}")
             exit()
 
-        return (nxt_type, val)
+        return nxt
+
+    def peek(self):
+        if self.next_token != None:
+            return self.next_token
+
+        self.next_token = next(self.generator)
+        return self.next_token
 
     def gen_tokens(self):
         inp = self.text
@@ -227,36 +189,117 @@ class Lexer():
                     break
 
             if last_success_token != -1:
-                if last_success_token in [0, 1, 5]:
-                    yield (Token(last_success_token), inp[:last_success_idx + 1])
-                else:
-                    yield (Token(last_success_token), None)
+                yield Token(TType(last_success_token), inp[:last_success_idx + 1])
 
                 inp = inp[last_success_idx + 1:]
             else:
                 print(f'ERROR: Invalid token "{inp}"')
                 exit()
 
-def replace(list_name, to_replace, replace_with):
-    idx = list_name.index(to_replace)
+class Parser():
+    def __init__(self, lexer):
+        self.lex = lexer
 
-    list_name = list_name[:idx] + replace_with + list_name[idx + 1:]
-    return list_name
+    def create_ast(self):
+        return self.parse_block()
 
-def reduce_parse_tree_to_ast(parse_tree_root, parent=None):
-    """reduce this node in the tree if it is reducable"""
-    ast = parse_tree_root
+    def parse_block(self):
+        self.lex.eat(TType.OPEN_CURLY)
 
-    children = ast.children.copy()
-    for child in children:
-        reduce_parse_tree_to_ast(child, ast)
+        children = []
+        children.append(self.parse_statement())
 
-    if ast.reduceable:
-        parent.children = replace(parent.children, ast, ast.children)
-        #for child in ast.children:
-         #   parent.add_child(child)
+        while self.lex.peek().type == TType.SEMICOLON:
+            self.lex.eat(TType.SEMICOLON)
+            children.append(self.parse_statement())
 
-    return ast
+        self.lex.eat(TType.CLOSED_CURLY)
+
+        return Block(children)
+
+    def parse_statement(self):
+        nxt = self.lex.peek()
+
+        if nxt.type == TType.OPEN_CURLY:
+            return self.parse_block()
+        elif nxt.type == TType.IF:
+            return self.parse_if_statement()
+        elif nxt.type == TType.PRINT:
+            return self.parse_print_statement()
+        else:
+            return self.parse_assignment()
+
+    def parse_if_statement(self):
+        self.lex.eat(TType.IF)
+        self.lex.eat(TType.OPEN_BRACE)
+
+        cond = self.parse_expr()
+
+        self.lex.eat(TType.CLOSED_BRACE)
+
+        block = self.parse_block()
+
+        return If_Statement(cond, block)
+
+    def parse_print_statement(self):
+        self.lex.eat(TType.PRINT)
+        self.lex.eat(TType.OPEN_BRACE)
+
+        expr = self.parse_expr()
+
+        self.lex.eat(TType.CLOSED_BRACE)
+
+        return Print(expr)
+
+    def parse_assignment(self):
+        ident = self.lex.eat(TType.IDENT).val
+        self.lex.eat(TType.EQUAL)
+
+        expr = self.parse_expr()
+
+        return Assignment(Var(ident), expr)
+
+    def parse_expr(self):
+        node = self.parse_expr1()
+
+        while self.lex.peek().type == TType.BIN_OP_EQ_NOTEQ:
+            op = self.lex.eat(TType.BIN_OP_EQ_NOTEQ).val
+
+            node = Operator(node, op, self.parse_expr1())
+
+        return node
+
+    def parse_expr1(self):
+        node = self.parse_expr2()
+
+        while self.lex.peek().type == TType.BIN_OP_PLUS_MIN:
+            op = self.lex.eat(TType.BIN_OP_PLUS_MIN).val
+            node = Operator(node, op, self.parse_expr2())
+
+        return node
+
+    def parse_expr2(self):
+        node = self.parse_val()
+
+        while self.lex.peek().type == TType.BIN_OP_MULT_DIV:
+            op = self.lex.eat(TType.BIN_OP_MULT_DIV).val
+            node = Operator(node, op, self.parse_val())
+
+        return node
+
+    def parse_val(self):
+        nxt = self.lex.peek()
+
+        node = None
+        if nxt.type == TType.IDENT:
+            ident_tok = self.lex.eat(TType.IDENT)
+            node = Var(ident_tok.val)
+        elif nxt.type == TType.NUMBER:
+            num = self.lex.eat(TType.NUMBER).val
+            node = Number(num)
+
+        return node
+
 
 def execute(tree_root):
     symbol_table = {}
@@ -268,9 +311,14 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         with open(sys.argv[1], 'r') as file:
             inp = file.read().replace('\n', '')
-            print (inp)
     else:
         inp = input()
 
     lexer = Lexer(inp)
-    print(lexer.eat(Token.IF))
+    parser = Parser(lexer)
+
+    ast = parser.create_ast()
+
+    symbol_table = {}
+
+    ast.exec(symbol_table)
